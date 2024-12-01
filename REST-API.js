b8afdaabfc5838c3342392d25179b9f58212d8b5
PammeC <pamelajnth@gmail.com>
2024-12-01 15:28:25 -0500

    feat: :alien: Add new program REST-API

--- REST-API.js
+++ REST-API.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.get('/api/users', (req, res) => {
     const users = [
         { id: 1, name: 'John' },
-        { id: 2, name: 'Jane' }
+        { id: 2, name: 'Janee' }
     ];
     res.json(users);
 });
